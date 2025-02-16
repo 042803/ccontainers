494f6c9a8d0eea03f5a766f80b6d24ea90a0add5
042803 <augustforstudies@gmail.com>
2025-02-16 12:55:23 +0300

    Sending array.c with new function emplace_at

--- array.c
+++ array.c
@@ -20,12 +20,19 @@ void push_back(struct Array* arr, int elt){
 }
 
 void emplace_at(struct Array* arr, int index, int elt){
-
-
+	if((index >= 0) && (index < arr->length)){ 
+		for (int i = arr->length; i > index; i--){
+			arr->A[i] = arr->A[i - 1]; 
+		}
+		arr->A[index] = elt;
+		arr->length++;	
+	}
 }
+
+
 int main(){	
 	struct Array arr = {{1, 2, 3, 4, 5}, 5, 10};
-	push_back(&arr, 6);
+	emplace_at(&arr, -3, 0); // 0 1 2 3 4 5 is expected 
 	print(arr);
 
 	return 0;			
