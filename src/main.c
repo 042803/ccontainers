1531ef6e9cc7045ce2d6ac59cdf1d80fec953fec
042803 <augustforstudies@gmail.com>
2025-04-18 17:23:59 +0300

    everything is  working!

--- src/main.c
+++ src/main.c
@@ -1,12 +1,8 @@
 #include "stdio.h"
 #include "../include/array.h"
+#include "../include/array_utils.h"
 
 int main(){
-	struct Array arr = {{1, 3, 5}, 3, 5};
-	struct Array arr2 = {{}, 0,5};
-	struct Array* arr3 = dif(&arr, &arr2);
-	print(*arr3);
-	int temp_length = arr3->length;
-	printf("%d\n", temp_length);
-	return 0;
+    printf("hello world!\n");
+    return 0; 
 }
