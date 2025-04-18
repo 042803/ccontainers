2664c88baf8668f6aab9c3e1f4e6048720213750
042803 <augustforstudies@gmail.com>
2025-04-18 23:30:57 +0300

    minor changes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "../include/array.h"
 #include "../include/array_utils.h"
+#include "../include/search.h"
+#include "../include/math.h"
+#include "../include/binop.h"
+#include "../include/msc.h"
+#include "../include/algo.h"
 
 int main(){
     struct Array arr1 = init(10);
