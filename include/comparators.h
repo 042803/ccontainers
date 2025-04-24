ed7a76604aabded62df0f47d97b9a83f8f71682e
042803 <augustforstudies@gmail.com>
2025-04-24 19:44:13 +0200

    all quiet on the western front

--- include/comparators.h
+++ include/comparators.h
@@ -3,9 +3,9 @@
 
 typedef int (*comparator_fn)(const void*, const void*);
 
-int cmp_ascending(const int* a, const int* b);
-int cmp_descending(const int* a, const int* b);
-int cmp_modulus(const int* a, const int* b);
-int cmp_even_first(const int* a, const int* b);
+int cmp_ascending(const void* a, const void* b);
+int cmp_descending(const void* a, const void* b);
+int cmp_modulus(const void* a, const void* b);
+int cmp_even_first(const void* a, const void* b);
 
 #endif
