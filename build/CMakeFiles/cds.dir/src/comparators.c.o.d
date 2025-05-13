e2c64ee05a7c3abf16861e22277a927e492339c2
042803 <augustforstudies@gmail.com>
2025-05-13 13:14:40 +0200

    remove build directory from version control and add to .gitignore

--- build/CMakeFiles/cds.dir/src/comparators.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/cds.dir/src/comparators.c.o: \
- /home/gus/dev/ccontainers/vector/src/comparators.c \
- /usr/include/stdc-predef.h \
- /home/gus/dev/ccontainers/vector/src/../include/comparators.h
